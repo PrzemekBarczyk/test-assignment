89e2fa4765d0ae8d30849508da7aa8a543e5d3e1
PrzemekBarczyk <przemek.barczyk@gmail.com>
2024-01-10 12:55:56 +0100

    Modify GroupDestinationIndicator
    When enabled, indicator shrinks and becomes disabled for certain period of time

--- Assets/Scripts/GroupDestinationIndicator.cs
+++ Assets/Scripts/GroupDestinationIndicator.cs
@@ -5,14 +5,34 @@ public class GroupDestinationIndicator : MonoBehaviour
 {
     [SerializeField] float _activeTimeInSeconds = 1f;
 
-    private void OnEnable()
+    private Vector3 _startScale;
+
+    private void Awake()
     {
-        StartCoroutine(WaitAndDisableCoroutine());
+        _startScale = transform.localScale;
     }
 
-    private IEnumerator WaitAndDisableCoroutine()
+    private void OnEnable()
     {
-        yield return new WaitForSeconds(_activeTimeInSeconds);
+        transform.localScale = _startScale;
+        StartCoroutine(ShrinkAndDisableCoroutine());
+    }
+
+    private IEnumerator ShrinkAndDisableCoroutine()
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < _activeTimeInSeconds)
+        {
+            transform.localScale = Vector3.Lerp(_startScale, Vector3.zero, elapsedTime / _activeTimeInSeconds);
+
+            elapsedTime += Time.deltaTime;
+
+            yield return null;
+        }
+
+        transform.localScale = Vector3.zero;
+
         gameObject.SetActive(false);
     }
 }
